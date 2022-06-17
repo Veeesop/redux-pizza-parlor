77d1cee020a5553a5e44fd493fd121bbeb373c7b
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-17 11:31:28 -0500

    added styling for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
@@ -26,6 +27,13 @@ function Header() {
         <>
             <header className='App-header'>
                 <h1 className='App-title'>Prime Pizza</h1>
+                <div>
+                    <Router>
+                        <Link to="/">
+                            <button className='home_btn'>Home</button>
+                        </Link>
+                    </Router>
+                </div>
                 <div className='cart_total'>
                     🛒 : $ {totalCart(cart)}
                 </div>
